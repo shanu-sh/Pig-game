7c7bc5e69759cee4f7f6f626fabf4219c50322d2
shanu shrivastava <shanu.shrivastava@students.iiit.ac.in>
2020-12-10 16:18:15 +0530

    resetting the game

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ score1El.textContent = 0;
 
 diceEl.classList.add('hidden')
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -72,3 +72,18 @@ btnHold.addEventListener('click', function () {
 
 });
 
+btnNew.addEventListener('click', function () {
+    scores = [0, 0];
+    btnHold.classList.remove('hidden');
+    btnRoll.classList.remove('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+    activePlayer = 0;
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+});
